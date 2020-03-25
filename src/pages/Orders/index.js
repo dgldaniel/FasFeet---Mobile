bf4f3aba7fcb64943dead5823eac8f6b0849b265
Daniel <dgl.daniel@outlook.com.br>
2020-03-24 21:14:03 -0300

    Config TabNavigator on Order and Profile

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -3,13 +3,13 @@ import React from 'react';
 import Header from './Header';
 import Delivery from './Delivery';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Orders() {
   return (
-    <>
+    <Container>
       <Header />
       <Delivery />
-    </>
+    </Container>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,64 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import SignIn from './pages/SignIn';
 import Orders from './pages/Orders';
 import Profile from './pages/Profile';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Dashboard() {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#7159c1',
+      }}
+    >
+      <Tab.Screen
+        name="Orders"
+        component={Orders}
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Entregas',
+          tabBarIcon: ({ color }) => (
+            <Icon name="format-align-justify" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Meu Perfil',
+          tabBarIcon: ({ color }) => (
+            <Icon name="account-circle" size={20} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Profile">
+      <Stack.Navigator initialRouteName="Dashboard">
         <Stack.Screen
           name="SignIn"
           component={SignIn}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name="Orders"
-          component={Orders}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={Profile}
+          name="Dashboard"
+          component={Dashboard}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
