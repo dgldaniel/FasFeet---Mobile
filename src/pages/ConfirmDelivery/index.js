1da8666a5df05ed6d5d9610f086cc1173c82c659
Daniel <dgl.daniel@outlook.com.br>
2020-03-27 23:18:08 -0300

    Implements ConfirmDelivery page

--- src/pages/ConfirmDelivery/index.js
+++ src/pages/ConfirmDelivery/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import {
+  ContainerImage,
+  ContainerCameraIcon,
+  CameraIcon,
+  SubmitButton,
+} from './styles';
 
 import DetailsLayout from '~/components/DetailsLayout';
 
 export default function ConfirmDelivery() {
-  return <DetailsLayout />;
+  return (
+    <DetailsLayout>
+      <ContainerImage>
+        <ContainerCameraIcon onPress={() => {}}>
+          <CameraIcon name="camera" />
+        </ContainerCameraIcon>
+      </ContainerImage>
+
+      <SubmitButton onPress={() => {}}>Enviar</SubmitButton>
+    </DetailsLayout>
+  );
 }
--- src/pages/ConfirmDelivery/styles.js
+++ src/pages/ConfirmDelivery/styles.js
@@ -1,3 +1,30 @@
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export const Container = styled.View``;
+import Button from '~/components/Button';
+
+export const ContainerImage = styled.View`
+  height: 70%;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 10px;
+  background-color: #7159c1;
+`;
+
+export const ContainerCameraIcon = styled.TouchableOpacity`
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  background-color: rgba(0, 0, 0, 0.4);
+
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CameraIcon = styled(Icon).attrs({
+  size: 30,
+  color: '#fff',
+})``;
